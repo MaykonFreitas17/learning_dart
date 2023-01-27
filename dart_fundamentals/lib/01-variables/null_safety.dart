97fac09b8271ed95101f7609a993267267cd80c1
Maykon Freitas <maykon17programmer@gmail.com>
2023-01-27 10:44:01 -0300

    feat: adiciona o arquivo null_safety.dart

---
+++ dart_fundamentals/lib/01-variables/null_safety.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  // Null Safety - Nulo Seguro
+  String? exemplo; // Utilizando ? depois do nome da váriavel, dizemos que ela pode ser nula.
+  print(exemplo); // Resultado -> null
+}
