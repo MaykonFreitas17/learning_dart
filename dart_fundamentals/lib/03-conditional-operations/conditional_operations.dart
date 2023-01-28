8f75c903281410aefa000ab2878f053f3cfc32a0
Maykon Freitas <maykon17programmer@gmail.com>
2023-01-28 10:34:56 -0300

    feat: adiciona a pasta e o arquivo conditional_operation.dart

---
+++ dart_fundamentals/lib/03-conditional-operations/conditional_operations.dart
@@ -0,0 +1,35 @@
+void main(List<String> args) {
+  // Operações de condiocionais
+  
+  /**
+   * Sintaxe
+   * if (condição) {
+   *  Operação caso a condição seja verdadeira (true)
+   * }
+   */
+
+  bool isAlive = true;
+  // Forma completa
+  if (isAlive) {
+    print("Está vivo!");
+  }
+
+  // Forma reduzida - Para uma única condição
+  if (isAlive) print("Está vivo!");
+
+  /**
+   * Sintaxe
+   * if (condição) {
+   *  Operação caso a condição seja verdadeira (true)
+   * } else {
+   *  Operação caso a condição não seja verdadeira (false)
+   * }
+   */
+
+//   isAlive = false;
+//   if (isAlive) {
+//     print("Está vivo!");
+//   } else {
+//     print("Está Morto!");
+//   }
+}
